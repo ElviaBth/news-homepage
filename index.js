93f205c423a3e2ddcd09ac03bb72f7b983d60fe2
Elvia Benedith <ejbenedith@gmail.com>
2022-11-07 22:44:03 +0100

    Add hamburger menu js

---
+++ index.js
@@ -0,0 +1,8 @@
+function createMenu(){
+    let menu =document.getElementById('myMenu');
+    if (menu.style.display==="block"){
+        menu.style.display= "none";
+    }else{
+        menu.style.display= "block";
+    }
+}
