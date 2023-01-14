d6b1d8a55cd8f0b5e69b89775f171eea067c27b4
Elvia Benedith <ejbenedith@gmail.com>
2023-01-14 20:11:13 +0100

    refactor package.json

--- index.js
+++
@@ -1,8 +0,0 @@
-function createMenu(){
-    let menu =document.getElementById('myMenu');
-    if (menu.style.display==="flex"){
-        menu.style.display= "none";
-    }else{
-        menu.style.display= "flex";
-    }
-}
